9085da074440061198201aa64a68a1af86b1640f
Lee@ubn <qq416606903@hotmail.com>
2024-04-15 18:43:35 +0800

    初始化项目，并安装gin config等并配置

--- main.go
+++ main.go
@@ -1,1 +1,35 @@
-package goapi
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go-api/bootstrap"
+	btsConfig "go-api/config"
+	"go-api/pkg/config"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	// 加载 config 目录下的配置信息
+	btsConfig.Initialize()
+}
+
+func main() {
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
+	// 初始化Gin 实例
+	r := gin.New()
+	// 初始化路由绑定
+	bootstrap.SetupRoute(r)
+
+	// 运行服务
+	err := r.Run(":" + config.Get("app.port"))
+	if err != nil {
+		//错误处理，端口被占用了或其他错误
+		fmt.Println(err.Error())
+	}
+}
